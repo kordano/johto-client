7755c18ae4d866386ca2a7ef2e3d60d4471ba819
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2019-03-19 18:28:58 +0100

    fiat lux

---
+++ src/main/johto_client/core.cljs
@@ -0,0 +1,27 @@
+(ns johto-client.core
+  (:require [reagent.core :as r]
+            ["@material-ui/core" :refer [Button]]))
+
+(def click-count (r/atom 0))
+
+(defn simple-component []
+  [:div
+   [:p "The state has a value: " @click-count]
+   [:> Button {:variant "contained" :color "primary" :on-click #(swap! click-count inc)} "Click!"]])
+
+
+(defn init []
+  (r/render
+    [simple-component]
+    (js/document.getElementById "root")))
+
+
+(comment
+
+  @click-count
+
+  (r/render
+    [simple-component]
+    (js/document.getElementById "root"))
+
+  )
